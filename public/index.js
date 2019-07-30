5b8cd30109da2b7c162103a073a3ed8c95b6e862
Rain Holloway <rainh2001@gmail.com>
2019-07-30 18:18:07 +1000

    View companies in comparison to input string

--- public/index.js
+++ public/index.js
@@ -1,5 +1,11 @@
 function queryStocks(){
-    var xhttp = new XMLHttpRequest();
+    var xhttp;
+    if (window.XMLHttpRequest) {
+        xhttp = new XMLHttpRequest();
+    }
+    else {
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
     xhttp.onreadystatechange = function(){
         if(this.readyState === 4 && this.status === 200){
             displayStocks(JSON.parse(this.response));
@@ -11,5 +17,51 @@ function queryStocks(){
 }
 
 function displayStocks(results){
-    console.log(results);
+    removeChildren(document.querySelector("#companies"));
+    results.forEach(company => {
+        let container = document.createElement("div");
+        container.classList.add("company");
+        if(company.name){
+            let name = document.createElement("div");
+            name.textContent = `Name: ${company.name}`;
+            container.appendChild(name);
+        }
+        if(company.symbol){
+            let symbol = document.createElement("div");
+            symbol.textContent = `Symbol: ${company.symbol}`;
+            container.appendChild(symbol);
+        }
+        if(company.market){
+            let market = document.createElement("div");
+            market.textContent = `Market: $${company.market}`;
+            container.appendChild(market);
+        }
+        if(company.price){
+            let price = document.createElement("div");
+            price.textContent = `Price: $${company.price}`;
+            container.appendChild(price);
+        }
+        if(company.change){
+            let change = document.createElement("div");
+            change.textContent = `Change: `;
+            let change_span = document.createElement("span");
+            if(company.change >= 0){
+                change_span.textContent += "+";
+                change_span.classList.add("green");
+            } else {
+                change_span.classList.add("red");
+            }
+            change_span.textContent += company.change;
+            change.appendChild(change_span);
+            container.appendChild(change);
+            container.remove
+        }
+        document.querySelector("#companies").appendChild(container);
+    });
+}
+
+function removeChildren(element){
+    while(element.firstChild){
+        element.removeChild(element.firstChild);
+    }
 }
