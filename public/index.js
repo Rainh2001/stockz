0ed8ae43e0b0a721c4e508c5a7cfbb000abdce1b
Rain Holloway <rainh2001@gmail.com>
2019-07-31 11:00:40 +1000

    Added function for adding company DOM

--- public/index.js
+++ public/index.js
@@ -10,6 +10,7 @@ function queryStocks(){
     }
     xhttp.onreadystatechange = function(){
         if(this.readyState === 4 && this.status === 200){
+            document.querySelector("button").disabled = false;
             if(this.response === "error"){
                 displayError(originalQuery);
             } else {
@@ -23,47 +24,9 @@ function queryStocks(){
 }
 
 function displayStocks(results){
-    document.querySelector("button").disabled = false;
     removeChildren(document.querySelector("#companies"));
     results.forEach(company => {
-        let container = document.createElement("div");
-        container.classList.add("company");
-        if(company.name){
-            let name = document.createElement("div");
-            name.textContent = `Name: ${company.name}`;
-            container.appendChild(name);
-        }
-        if(company.symbol){
-            let symbol = document.createElement("div");
-            symbol.textContent = `Symbol: ${company.symbol}`;
-            container.appendChild(symbol);
-        }
-        if(company.market){
-            let market = document.createElement("div");
-            market.textContent = `Market: $${company.market}`;
-            container.appendChild(market);
-        }
-        if(company.price){
-            let price = document.createElement("div");
-            price.textContent = `Price: $${company.price}`;
-            container.appendChild(price);
-        }
-        if(company.change){
-            let change = document.createElement("div");
-            change.textContent = `Change: `;
-            let change_span = document.createElement("span");
-            if(company.change >= 0){
-                change_span.textContent += "+";
-                change_span.classList.add("green");
-            } else {
-                change_span.classList.add("red");
-            }
-            change_span.textContent += company.change;
-            change.appendChild(change_span);
-            container.appendChild(change);
-            container.remove
-        }
-        document.querySelector("#companies").appendChild(container);
+        addCompany(company);
     });
 }
 
@@ -79,5 +42,45 @@ function displayError(query){
     errorMessage.classList.add("error");
     errorMessage.textContent = `Couldn't retrieve any stocks referencing: ${query}`;
     document.querySelector("#companies").appendChild(errorMessage);
-    document.querySelector("button").disabled = false;
+}
+
+function addCompany(company){
+    let container = document.createElement("div");
+    container.classList.add("company");
+    if(company.name){
+        let name = document.createElement("div");
+        name.textContent = `Name: ${company.name}`;
+        container.appendChild(name);
+    }
+    if(company.symbol){
+        let symbol = document.createElement("div");
+        symbol.textContent = `Symbol: ${company.symbol}`;
+        container.appendChild(symbol);
+    }
+    if(company.market){
+        let market = document.createElement("div");
+        market.textContent = `Market: $${company.market}`;
+        container.appendChild(market);
+    }
+    if(company.price){
+        let price = document.createElement("div");
+        price.textContent = `Price: $${company.price}`;
+        container.appendChild(price);
+    }
+    if(company.change){
+        let change = document.createElement("div");
+        change.textContent = `Change: `;
+        let change_span = document.createElement("span");
+        if(company.change >= 0){
+            change_span.textContent += "+";
+            change_span.classList.add("green");
+        } else {
+            change_span.classList.add("red");
+        }
+        change_span.textContent += company.change;
+        change.appendChild(change_span);
+        container.appendChild(change);
+        container.remove
+    }
+    document.querySelector("#companies").appendChild(container);
 }
